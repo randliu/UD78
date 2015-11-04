88a7a6573aaea057c3dea60c1a0d5a96f0707c30
randliu <buckler.liu@gmail.com>
2015-11-04 14:39:53 +0800

    day default

--- src/hammar/management/commands/report.py
+++ src/hammar/management/commands/report.py
@@ -7,7 +7,7 @@ from optparse import make_option
 class Command(BaseCommand):
 	option_list = BaseCommand.option_list+(\
 		make_option('-m','--market',dest="market",action='store',type='string',help='sh/sz/hk'),\
-		make_option('-d','--day',dest="day",action='store',type='int',help='days'),\
+		make_option('-d','--day',dest="day",default=3,action='store',type='int',help='days'),\
 		make_option('-i','--index',dest="index",default=False,action='store_true',help=u'各个市场的指数'),\
 		make_option('-c','--code',dest="code",default=0,action='store',type='int',help=u'股票号码'),\
 		)
